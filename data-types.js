fc66ec5aa0e3b09dbb9f9bbf84de009fb26c6e0d
Tessenjutsu <deleonwilliams36@gmail.com>
2024-10-21 09:19:52 -0400

    first commit

---
+++ data-types.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let greeting = "Hello"; // variables with let cannot be redeclared.
+let force = 37;
+let speed = 237;
+let contact = 667;
+let result = force + speed * contact;
+console.log(result);
---
+++ if.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let day = "friday";
+let condition = day == "monday";
+
+if (condition){
+    console.log("log out, condition was true")
+}else{
+    console.log("log out, condition was false")
+}
